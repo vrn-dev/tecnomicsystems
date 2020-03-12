5759296b41d8fd135edd3c5f72074e2064d18a16
Vaisakh Nair <vrn.dev@outlook.com>
2020-03-12 18:29:18 +0400

    css split up back to top made native

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="antialiased">
-    <back-to-top bottom="50px" right="50px">
-      <button
-        aria-label="Back to Top Button"
-        class="bg-purple-600 hover:bg-pink-400 text-white font-bold py-2 px-4 rounded-full"
-      >
-        <img
-          class="lazyload"
-          alt="Back to Top Button"
-          data-src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiIgdmlld0JveD0iMCAwIDMyIDMyIj48cGF0aCBkPSJNMTUuOTk3IDEzLjM3NGwtNy4wODEgNy4wODFMNyAxOC41NGw4Ljk5Ny04Ljk5OCA5LjAwMyA5LTEuOTE2IDEuOTE2eiIvPjwvc3ZnPg=="
-        />
-      </button>
-    </back-to-top>
+    <transition name="back-to-top-fade">
+      <div v-show="visible" @click="backToTop" class="back-to-top">
+        <button
+          aria-label="Back to Top Button"
+          class="bg-purple-600 hover:bg-pink-400 text-white font-bold py-2 px-4 rounded-full"
+        >
+          <img
+            class="lazyload"
+            alt="Back to Top Button"
+            data-src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiIgdmlld0JveD0iMCAwIDMyIDMyIj48cGF0aCBkPSJNMTUuOTk3IDEzLjM3NGwtNy4wODEgNy4wODFMNyAxOC41NGw4Ljk5Ny04Ljk5OCA5LjAwMyA5LTEuOTE2IDEuOTE2eiIvPjwvc3ZnPg=="
+          />
+        </button>
+      </div>
+    </transition>
     <Navbar />
     <nuxt />
     <Footer />
@@ -19,15 +21,40 @@
 </template>
 
 <script>
-import BackToTop from '~/plugins/BackToTop.vue';
 import Navbar from '~/components/Navbar.vue';
 import Footer from '~/components/Footer.vue';
 
 export default {
   components: {
     Navbar,
-    Footer,
-    BackToTop
+    Footer
+  },
+  data: () => ({
+    visible: false
+  }),
+  mounted() {
+    this.smoothscroll();
+  },
+  destroyed() {
+    window.removeEventListener('scroll', this.catchScroll);
+  },
+  methods: {
+    catchScroll() {
+      const pastTopOffset = window.pageYOffset > 600;
+      this.visible = pastTopOffset;
+    },
+    backToTop() {
+      this.smoothscroll();
+    },
+    smoothscroll() {
+      const currentScroll =
+        document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > 0) {
+        window.requestAnimationFrame(this.smoothscroll);
+        window.scrollTo(0, Math.floor(currentScroll - currentScroll / 5));
+      }
+      window.addEventListener('scroll', this.catchScroll);
+    }
   }
 };
 </script>
