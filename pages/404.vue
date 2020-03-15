9d7ceb76ebc1ad3ff77b1a3f87d29538f3104f4e
Vaisakh Nair <vrn.dev@outlook.com>
2020-03-15 15:31:16 +0400

    thuraya updated firebase.json rewrites 404 page

---
+++ pages/404.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="container bg-blue-100">
+      <div class="flex flex-col">
+        <h1 class="subtitle">404</h1>
+        <main class="flex flex-col justify-center items-center content-center">
+          <div class="xs:p-5 md:p-16">
+            <h2 class="subtitle">
+              We could not find the page you were looking for...
+            </h2>
+          </div>
+        </main>
+        <div class="spacer"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Tecnomic | About Us'
+    };
+  }
+};
+</script>
+
+<style></style>
